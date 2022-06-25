eaff767d15aad34ff3e1e179a7f33b92b0e269a9
Fazlı Sercan Yılmaz <sercan.yilmaz@getir.com>
2022-06-25 19:50:24 +0300

    feat: added Config class

---
+++ src/Common/Config.ts
@@ -0,0 +1,38 @@
+import { IConfig } from "../Models/Domains/IConfig";
+import * as Joi from "joi";
+
+export class Config implements IConfig {
+  public readonly testCaseMinSize: number;
+  public readonly testCaseMaxSize: number;
+  public readonly matrixMaxRow: number;
+  public readonly matrixMinRow: number;
+  public readonly matrixMinColom: number;
+  public readonly matrixMaxColom: number;
+
+  constructor(env: any) {
+    this.testCaseMinSize = env.testCaseMinSize;
+    this.testCaseMaxSize = env.testCaseMaxSize;
+
+    this.matrixMaxRow = env.matrixMaxRow;
+    this.matrixMinRow = env.matrixMinRow;
+
+    this.matrixMinColom = env.matrixMinColom;
+    this.matrixMaxColom = env.matrixMaxColom;
+
+    const validationSchema = Joi.object().keys({
+      testCaseMinSize: Joi.number().default(1),
+      testCaseMaxSize: Joi.number().default(100),
+      matrixMaxRow: Joi.number().default(182),
+      matrixMinRow: Joi.number().default(1),
+      matrixMinColom: Joi.number().default(1),
+      matrixMaxColom: Joi.number().default(182),
+    });
+
+    const { error, value } = validationSchema.validate(this);
+
+    if (error) {
+      throw new Error("todo");
+    }
+    return value;
+  }
+}
---
+++ src/Models/Domains/IConfig.ts
@@ -0,0 +1,8 @@
+export interface IConfig {
+  testCaseMinSize: number;
+  testCaseMaxSize: number;
+  matrixMaxRow: number;
+  matrixMinRow: number;
+  matrixMinColom: number;
+  matrixMaxColom: number;
+}
